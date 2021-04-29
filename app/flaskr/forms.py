7482deb0fc833765c895aecd1aa99af872218cca
Edward Chamberlain <edward.chamberlain@me.com>
2021-04-29 22:42:54 +0100

    tidied up forms

--- app/flaskr/forms.py
+++ app/flaskr/forms.py
@@ -2,9 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, IntegerField
 from wtforms.validators import DataRequired, NumberRange
 
+
 class Fanspeed_Form(FlaskForm):
     speed = IntegerField('Speed Percentage', validators=[NumberRange(min=10, max=100, message="Value must be between 10% and 100%"), DataRequired()])
     submit = SubmitField('Apply')
+
 
 class Configure_Form(FlaskForm):
     host = StringField("IPMI Hostname")
@@ -12,6 +14,7 @@ class Configure_Form(FlaskForm):
     passwd = PasswordField("IPMI Password")
     
     submit_configure = SubmitField('Apply')
+
 
 class StartUp_Form(FlaskForm):
     enable = BooleanField("Enable Startup Script")
