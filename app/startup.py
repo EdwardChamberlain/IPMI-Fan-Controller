3f6e351b1fe5cfc262a9f9fb716ba806627f8367
Edward Chamberlain <edward.chamberlain@me.com>
2021-04-29 22:18:37 +0100

    Created start up

--- app/startup.py
+++ app/startup.py
@@ -1,4 +1,31 @@
+import json
+
 import logging
 import config
+import ipmitools
 
-# Startup script to go here.
+template = {
+    'ENABLE': 'False',
+    'MANUAL_MODE': 'False',
+    'FAN_SPEED': ''
+}
+
+try:
+    path = '/data/startup.json' if not config.DEVELOP else 'data/startup.json'
+    with open(path, 'r') as f:
+        startup_script = json.load(f)
+except:
+    logging.error("Error opening startup script - Generating new script")
+    with open(path, 'w') as f:
+        json.dump(template, f)
+    exit()
+
+if startup_script['ENABLE'] == 'True':
+
+    if startup_script['MANUAL_MODE'] == 'True':
+        logging.info("Setting Manual Mode")
+        ipmitools.set_manual_mode()
+
+    if startup_script['FAN_SPEED']:
+        logging.info(f"Setting Fan Speed: {startup_script['FAN_SPEED']}")
+        ipmitools.set_fan_speed(startup_script['FAN_SPEED'])
